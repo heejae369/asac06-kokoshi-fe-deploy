826c1c650e03581c4025530f96e257e93c991151
LunarClover <twojjune@naver.com>
2024-12-12 18:10:25 +0900

    feat: Windows 환경에서 dev-json-server 동작하게 package.json 수정

---
+++ app/users/login/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex h-screen w-full justify-center bg-gray-100">
+      <div className="w-[360px] bg-white">test</div>
+    </div>
+  );
+}
