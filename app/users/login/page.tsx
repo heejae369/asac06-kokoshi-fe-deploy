394bf4ee59509d41ee11044c80754d7a2cf9257c
LunarClover <twojjune@naver.com>
2024-12-16 15:55:32 +0900

    feat: #16 CSS 수정 및 components 일부 추가 구현

--- app/users/login/page.tsx
+++ app/users/login/page.tsx
@@ -6,13 +6,12 @@ export default function LoginPage() {
   return (
     <div>
       <div className="flex h-screen w-full justify-center bg-gray-100">
-        <div className="w-[360px] bg-white">
+        <div className="relative w-[360px] bg-white">
           <div className="login">로그인</div>
-          <Input className="frame4" />
-          <>
-            <Input className="frame5" />
-            <Button className="ic_pw_hide" />
-          </>
+          <Input className="frame4" placeholder="이메일" />
+          <Input className="frame5" placeholder="비밀번호" />
+          <Button className="ic_pw_hide" />
+          <div className="frame6">로그인</div>
         </div>
       </div>
     </div>
