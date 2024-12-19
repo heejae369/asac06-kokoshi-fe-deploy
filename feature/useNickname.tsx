a545138655c0bedaaae27f6357ad5723ee1b1ec5
hoji920 <92128530@naver.com>
2024-12-19 23:03:33 +0900

    feat : 로컬스토리지 data  spring 서버로 post요청 구현

--- feature/useNickname.tsx
+++ feature/useNickname.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { useState, useEffect } from "react";
 import { generateRandomNickname } from "@/feature/generateRandomNickname";
-import { localStorageApi } from "@/lib/localStorageApi";
+// import { localStorageApi } from "@/lib/localStorageApi";
+import CustomFetch from "@/feature/CustomFetch";
 
 export const useNickname = () => {
   const [nickname, setNickname] = useState("");
@@ -61,12 +62,30 @@ export const useNickname = () => {
       );
       setNickname(randomNickname);
     } else {
-      // 닉네임 유효성이 통과되면 로컬 스토리지에 저장
-      saveNicknameToLocalStorage(nickname);
-
       alert(`닉네임이 저장되었습니다: ${nickname}`);
       console.log("닉네임:", nickname);
-      localStorageApi();
+    }
+    // 닉네임 유효성이 통과되면 로컬 스토리지에 저장
+    saveNicknameToLocalStorage(nickname);
+
+    //로컬스토리지 데이터 수집
+    if (typeof window !== "undefined") {
+      const keys = ["gender", "interest", "nickname"];
+      const requestData: Record<string, string> = {};
+      keys.forEach((key) => {
+        const value = localStorage.getItem(key);
+        if (value) {
+          requestData[key] = value; // JSON 형식이면 파싱
+        } else {
+          console.warn(
+            `로컬 스토리지에 '${key}'에 해당하는 데이터가 없습니다.`
+          );
+        }
+      });
+      // 최종 requestData 확인 (디버깅용)
+      console.log("최종 requestData:", requestData);
+
+      CustomFetch("/users/api", "POST", requestData);
     }
   };
 
