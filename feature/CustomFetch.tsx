f71f34a854ad6702e2d0d032a9d7dd5b7bbd67be
LunarClover <twojjune@naver.com>
2024-12-19 17:29:34 +0900

    feat: Fetch API 커스텀 구현

---
+++ feature/CustomFetch.tsx
@@ -0,0 +1,19 @@
+export default async function CustomFetch(path, method, request) {
+  if (request) {
+    // POST, PUT,
+    return fetch("http://localhost:8080" + path, {
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(request),
+    }).catch((error) => console.error("Error:", error));
+  } else {
+    // GET, DELETE
+    return await fetch("http://localhost:8080" + path, {
+      method: method,
+    })
+      .then((response) => response?.json())
+      .catch((error) => console.error("Error:", error));
+  }
+}
