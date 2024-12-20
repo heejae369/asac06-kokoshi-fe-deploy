4ba0a91c556a6be0826f6fe941a61af92643781b
hoji920 <92128530@naver.com>
2024-12-20 15:33:07 +0900

    fix : post api 요청 feature 디렉토리에 뺌

--- feature/useNickname.tsx
+++ feature/useNickname.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import { generateRandomNickname } from "@/feature/generateRandomNickname";
 // import { localStorageApi } from "@/lib/localStorageApi";
 import CustomFetch from "@/feature/CustomFetch";
+import { postApiSendingTest } from "@/feature/postApiSending";
 
 export const useNickname = () => {
   const [nickname, setNickname] = useState("");
@@ -67,26 +68,7 @@ export const useNickname = () => {
     }
     // 닉네임 유효성이 통과되면 로컬 스토리지에 저장
     saveNicknameToLocalStorage(nickname);
-
-    //로컬스토리지 데이터 수집
-    if (typeof window !== "undefined") {
-      const keys = ["gender", "interest", "nickname"];
-      const requestData: Record<string, string> = {};
-      keys.forEach((key) => {
-        const value = localStorage.getItem(key);
-        if (value) {
-          requestData[key] = value; // JSON 형식이면 파싱
-        } else {
-          console.warn(
-            `로컬 스토리지에 '${key}'에 해당하는 데이터가 없습니다.`
-          );
-        }
-      });
-      // 최종 requestData 확인 (디버깅용)
-      console.log("최종 requestData:", requestData);
-
-      CustomFetch("/users/api", "POST", requestData);
-    }
+    postApiSendingTest();
   };
 
   return {
