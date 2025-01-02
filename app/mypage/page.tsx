22415b50887729fa81ccc478789252be951a390a
hoji920 <92128530@naver.com>
2025-01-02 10:22:57 +0900

    feat : footer 라우팅시 해당 이모티콘 색상 변경

---
+++ app/mypage/page.tsx
@@ -0,0 +1,10 @@
+import Footer from "@/components/Footer";
+
+export default function MyPage() {
+  return (
+    <div>
+      <p>마이페이지</p>
+      <Footer />
+    </div>
+  );
+}
--- components/Footer.js
+++ components/Footer.js
@@ -1,63 +1,81 @@
 "use client";
 import Link from "next/link";
-import { useRouter } from "next/navigation"; // 현재 경로를 가져오기 위한 useRouter
+import { usePathname } from "next/navigation"; // 현재 경로를 가져오기 위한 usePathname
 import styles from "../styles/Footer.module.css";
-import mainPage from "@/assets/icon/smiley-happy.png";
+import myPage from "@/assets/icon/ic_menu_mypage_p.png";
 import goHome from "@/assets/icon/home-02.png";
-import map from "@/assets/icon/ic_menu_map.png";
+import map from "@/assets/icon/ic_menu_map_p.png";
 
 const Footer = () => {
-  const router = useRouter(); // 현재 경로 확인
+  const pathname = usePathname(); // 현재 경로 가져오기
 
   return (
     <footer className={styles.footer}>
       <nav className={styles.nav}>
         {/* 지도 */}
-        <Link href="/map" className={styles.navItem}>
+        <Link href="/map">
           <div
-            className={`${styles.icon} ${
-              router.pathname === "/map" ? styles.active : ""
+            className={`${styles.navItem} ${
+              pathname === "/map" ? styles.active : ""
             }`}
           >
-            <img src={map.src} alt="지도" />
+            <img
+              src={map.src}
+              alt="지도"
+              className={`${styles.icon} ${
+                pathname === "/map" ? styles.activeIcon : ""
+              }`}
+            />
+            <span className={`${pathname === "/map" ? styles.activeText : ""}`}>
+              지도
+            </span>
           </div>
-          <span
-            className={`${router.pathname === "/map" ? styles.active : ""}`}
-          >
-            지도
-          </span>
         </Link>
 
         {/* 홈 */}
-        <Link href="/yanolza/main" className={styles.navItem}>
+        <Link href="/yanolza/main">
           <div
-            className={`${styles.icon} ${
-              router.pathname === "/yanolza/main" ? styles.active : ""
+            className={`${styles.navItem} ${
+              pathname === "/yanolza/main" ? styles.active : ""
             }`}
           >
-            <img src={goHome.src} alt="홈" />
+            <img
+              src={goHome.src}
+              alt="홈"
+              className={`${styles.icon} ${
+                pathname === "/yanolza/main" ? styles.activeIcon : ""
+              }`}
+            />
+            <span
+              className={`${
+                pathname === "/yanolza/main" ? styles.activeText : ""
+              }`}
+            >
+              홈
+            </span>
           </div>
-          <span
-            className={`${router.pathname === "/yanolza/main" ? styles.active : ""}`}
-          >
-            홈
-          </span>
         </Link>
 
         {/* 마이페이지 */}
-        <Link href="/mypage" className={styles.navItem}>
+        <Link href="/mypage">
           <div
-            className={`${styles.icon} ${
-              router.pathname === "/mypage" ? styles.active : ""
+            className={`${styles.navItem} ${
+              pathname === "/mypage" ? styles.active : ""
             }`}
           >
-            <img src={mainPage.src} alt="마이페이지" />
+            <img
+              src={myPage.src}
+              alt="마이페이지"
+              className={`${styles.icon} ${
+                pathname === "/mypage" ? styles.activeIcon : ""
+              }`}
+            />
+            <span
+              className={`${pathname === "/mypage" ? styles.activeText : ""}`}
+            >
+              마이페이지
+            </span>
           </div>
-          <span
-            className={`${router.pathname === "/mypage" ? styles.active : ""}`}
-          >
-            마이페이지
-          </span>
         </Link>
       </nav>
     </footer>
