2e7e3743b1467ecd48dcbe751ba18916a6bded54
LunarClover <twojjune@naver.com>
2025-01-20 14:52:37 +0900

    feat: #61 호텔 카테고리 페이지의 이벤트 배너 autoplay 기능 구현

--- components/yanolza/hotelCategory/EventBanner.tsx
+++ components/yanolza/hotelCategory/EventBanner.tsx
@@ -1,7 +1,7 @@
 import "swiper/css";
 import "swiper/css/pagination";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
+import { Pagination, Autoplay } from "swiper/modules";
 import hotel_banner from "@/assets/img/image_hotel_banner.png";
 import { useRouter } from "next/navigation";
 
@@ -11,7 +11,7 @@ export default function EventBanner() {
     <>
       <div className="absolute left-1/2 w-[360px] -translate-x-1/2">
         <Swiper
-          modules={[Pagination]}
+          modules={[Pagination, Autoplay]}
           spaceBetween={0} // 슬라이드 간 간격
           slidesPerView={1} // 한 번에 보여지는 슬라이드 수
           pagination={{
@@ -19,6 +19,7 @@ export default function EventBanner() {
             el: ".swiper-pagination",
           }}
           loop={true} // 슬라이드 반복
+          autoplay={true}
         >
           <SwiperSlide>
             <button onClick={() => router.push("/yanolza/event")}>
